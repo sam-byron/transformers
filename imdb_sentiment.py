dfe8607c65eaa3db66ae5722360241c4a1285c1b
Sam Byron <mrc4090@proton.me>
2024-02-28 09:41:59 +0200

    moved model inference outside of loop, yet time to train decreases as batch size decreases which makes no sense

--- imdb_sentiment.py
+++ imdb_sentiment.py
@@ -9,8 +9,9 @@ from torch.utils.data import DataLoader
 # from torchdata.dataloader2 import DataLoader2
 import time
 from torchinfo import summary
-from transformers import GPT2Tokenizer
+from transformers import GPT2Tokenizer, GPT2TokenizerFast
 from transformers import GPT2Model
+from transformers import GPT2Config
 import torchtext
 from torchtext import __version__ as torchtext_version
 from pkg_resources import parse_version
@@ -38,47 +39,65 @@ torch.manual_seed(1)
 # train_dataset, valid_dataset = random_split(list(train_dataset), [20000, 5000])
 
 # DEBUG
-train_dataset, valid_dataset = random_split(list(train_dataset)[0:2500], [2000, 500])
+train_dataset, valid_dataset = random_split(list(train_dataset)[0:500], [400, 100])
 
 train_size = 20000
 valid_size = 5000
 test_size = 25000
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-gpt_model = GPT2Model.from_pretrained('gpt2')
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+config = GPT2Config.from_pretrained('gpt2')
+# Account for added pad token
+config.vocab_size = 50258
+gpt_model = GPT2Model(config)
+# gpt_model = GPT2Model.from_pretrained('gpt2')
 print(gpt_model)
 max_seq_len = gpt_model.config.n_positions
 
 encoded_input = tokenizer(train_dataset[0][-1], return_tensors='pt')
 out = gpt_model(**encoded_input)
 hidden_size = out['last_hidden_state'].shape[2]
+summary(gpt_model, input_data=[encoded_input["input_ids"]], device="cpu")
 
 def encode_transform_batch(device, tokenizer, model, hidden_size):
 
-    # text_pipeline = lambda x: [tokenizer(x)]
+    text_pipeline = lambda x: [sentence[1] for sentence in x]
     if parse_version(torchtext.__version__) > parse_version("0.10"):
-        label_pipeline = lambda x: 1. if x == 2 else 0.         # 1 ~ negative, 2 ~ positive review
+        label_pipeline = lambda x: 1. if x == 2 else 0.  # 1 ~ negative, 2 ~ positive review
     else:
         label_pipeline = lambda x: 1. if x == 'pos' else 0.
 
     def collate_fn(batch):
-        label_list = []
-        all_data_tensor = torch.empty((0, hidden_size), dtype=torch.float32)
-        all_data_tensor.to(device)
+        with torch.no_grad():
+            model.eval()
+            encoded_inputs = text_pipeline(batch)
+            encoded_inputs = tokenizer(encoded_inputs, return_tensors='pt', max_length = max_seq_len-1, padding=True, truncation=True)
+            encoded_inputs.to(device)
+            # model.to("cpu")
+            # encoded_inputs.to("cpu")
+            out = model(**encoded_inputs)
+            all_data = out['last_hidden_state'][:,-1,:]
+            label_list = []
+            # all_data_tensor = torch.empty((0, hidden_size), dtype=torch.float32)
+            # all_data_tensor = torch.cat((all_data_tensor.to(device), out['last_hidden_state'][:,-1,:].detach()), 0)
+            # all_data_tensor = torch.empty((0, max_seq_len-1, hidden_size), dtype=torch.float32)
+            # all_data_tensor = torch.cat((all_data_tensor.to(device), out['last_hidden_state'].detach()), 0)
+            # all_data_tensor.to(device)
         for _label, _text in batch:
             label_list.append(label_pipeline(_label))
-            encoded_input = tokenizer(_text, return_tensors='pt')
-            # processed_text = model(**encoded_input)
-            encoded_input.to(device)
-            processed_text = model(input_ids = encoded_input["input_ids"][:, :max_seq_len-1], attention_mask = encoded_input["attention_mask"][:, :max_seq_len-1])
-            # output = torch.tensor(processed_text['last_hidden_state'][:,-1,:])
-            output = processed_text['last_hidden_state'][:,-1,:].detach()
-            all_data_tensor = torch.cat((all_data_tensor.to(device), output), 0)
+            # encoded_input = tokenizer(_text, return_tensors='pt')
+            # # processed_text = model(**encoded_input)
+            # encoded_input.to(device)
+            # processed_text = model(input_ids = encoded_input["input_ids"][:, :max_seq_len-1], attention_mask = encoded_input["attention_mask"][:, :max_seq_len-1])
+            # # output = torch.tensor(processed_text['last_hidden_state'][:,-1,:])
+            # output = processed_text['last_hidden_state'][:,-1,:].detach()
+            # all_data_tensor = torch.cat((all_data_tensor.to(device), output), 0)
             # text_list.append(output.squeeze(0))
         label_list = torch.tensor(label_list)
         # padded_text_list = nn.utils.rnn.pad_sequence(text_list, batch_first=True)
         # return padded_text_list.to(device), label_list.to(device)
-        return all_data_tensor.to(device), label_list.to(device)
+        return all_data.to(device), label_list.to(device)
     
     return collate_fn
 
@@ -90,7 +109,7 @@ collate_fn = encode_transform_batch(device, tokenizer, gpt_model, hidden_size)
 
 ## Step 4: batching the datasets
 
-batch_size = 32*2  
+batch_size = 8
 
 train_dl = DataLoader(train_dataset, batch_size=batch_size,
                       shuffle=True, collate_fn=collate_fn)
@@ -102,13 +121,14 @@ test_dl = DataLoader(test_dataset, batch_size=batch_size,
 
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, fc_hidden_size, loss_fn):
+    def __init__(self, input_dim, fc_hidden_size, loss_fn, gpt_model):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, fc_hidden_size)
         self.fc2 = nn.Linear(fc_hidden_size, 1)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
         self.loss_fn = loss_fn
+        self.gpt_model = gpt_model
         # self.optimizer = optimizer
 
     def forward(self, input):
@@ -154,7 +174,7 @@ class MLP(nn.Module):
 
 
 loss_fn = nn.BCELoss()
-model = MLP(hidden_size, 1024, loss_fn)
+model = MLP(hidden_size, 1024, loss_fn, gpt_model)    
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001) 
 model.optimizer = optimizer
 
@@ -163,10 +183,12 @@ print(model)
  
 model.to(device)
 num_epochs = 1
+# model.train_epochs(num_epochs, train_dl, valid_dl, valid_size)
 
+# DEBUG
 with profile(activities=[
         ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
     with record_function("model_training"):
         model.train_epochs(num_epochs, train_dl, valid_dl, valid_size)
 
-print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=20))
